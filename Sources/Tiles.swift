f184c14074a35b534595f491736dfad3a5ee657c
vaux <zero.griffin@gmail.com>
2022-05-12 18:02:13 +0200

    Update max

--- Sources/Tiles.swift
+++ Sources/Tiles.swift
@@ -44,8 +44,8 @@ public struct Tiles: Storable {
     
     init(items: [UInt8 : [UInt32 : [UInt32 : Data]]]) {
         print("z \(items.count)")
-        print("x \(items.map { $0.value.map { $0.key }.count })")
-        print("y \(items.compactMap { $0.value.map { $0.value.map { $0.key }.count } })")
+        print("x \(items.map { $0.value.map { $0.key }.count }.max())")
+        print("y \(items.compactMap { $0.value.map { $0.value.map { $0.key }.count }.max() }.max())")
         
         self.items = items
     }
