94605a74cf58ec5ee4a679f657f81504dcbe003a
vaux <zero.griffin@gmail.com>
2022-05-12 17:48:34 +0200

    Update tiles

--- Sources/Tiles.swift
+++ Sources/Tiles.swift
@@ -44,9 +44,12 @@ public struct Tiles: Storable {
     
     init(items: [UInt8 : [UInt32 : [UInt32 : Data]]]) {
         print("z \(items.count)")
-        print("x \(items.map { $0.value.map { $0.key }.count }.max())")
-        print("y \(items.compactMap { $0.value.map { $0.value.map { $0.key }.count }.max() }.max())")
         
+        print("x \(items[19]!.count)")
+        
+        items[19]!.forEach {
+            print("y \($0.key) \($0.value.count)")
+        }
         self.items = items
     }
     
