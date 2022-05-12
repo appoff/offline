5f94d14cd0c9a9ea18286015fa0abc751d76efe0
vaux <zero.griffin@gmail.com>
2022-05-12 15:56:55 +0200

    Fix maxx

--- Sources/Array.CLLocationCoordinate2D.swift
+++ Sources/Array.CLLocationCoordinate2D.swift
@@ -6,7 +6,7 @@ extension Array where Element == CLLocationCoordinate2D {
     var rect: MKMapRect {
         { latitude, longitude in
             { min, max in
-                .init(x: min.x, y: min.y, width: max.y - min.x, height: max.y - min.y)
+                .init(x: min.x, y: min.y, width: max.x - min.x, height: max.y - min.y)
             } (MKMapPoint(.init(latitude: latitude.first!.latitude + margin, longitude: longitude.first!.longitude - margin)),
                MKMapPoint(.init(latitude: latitude.last!.latitude - margin, longitude: longitude.last!.longitude + margin)))
         } (sorted { $0.latitude > $1.latitude }, sorted { $0.longitude < $1.longitude })
