fe28e3ddaea98f4f087f612bc43c010a60c9bc3a
vaux <zero.griffin@gmail.com>
2022-05-12 16:33:38 +0200

    Update factory

--- Sources/Factory.swift
+++ Sources/Factory.swift
@@ -42,7 +42,7 @@ public final class Factory {
         print("Shoot")
         guard let next = shots.last else { return }
         progress.send((total - .init(shots.count)) / total)
-        timer.schedule(deadline: .now() + 10)
+//        timer.schedule(deadline: .now() + 10)
         
         let shooter = MKMapSnapshotter(options: next.options)
 //        self.shooter = shooter
