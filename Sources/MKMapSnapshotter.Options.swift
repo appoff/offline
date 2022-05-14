2faf40bb3f1c4b1bf2e8ce3b4beb0ec7a63696c2
vaux <zero.griffin@gmail.com>
2022-05-14 12:52:35 +0200

    Update settings

--- Sources/MKMapSnapshotter.Options.swift
+++ Sources/MKMapSnapshotter.Options.swift
@@ -2,10 +2,42 @@ import MapKit
 
 extension MKMapSnapshotter.Options {
     func configure(settings: Settings) -> Self {
+        pointOfInterestFilter = settings.interest ? .includingAll : .excludingAll
+        
+        switch settings.map {
+        case .standard:
+            mapType = .standard
+        case .satellite:
+            mapType = .satelliteFlyover
+        case .hybrid:
+            mapType = .hybridFlyover
+        case .emphasis:
+            mapType = .mutedStandard
+        }
+        
 #if os(iOS)
-        traitCollection = .init(traitsFrom: [.init(displayScale: 2), .init(userInterfaceStyle: .light)])
+        var traits = [UITraitCollection]()
+        traits.append(.init(displayScale: 2))
+        
+        switch settings.scheme {
+        case .auto:
+            break
+        case .light:
+            traits.append(.init(userInterfaceStyle: .light))
+        case .dark:
+            traits.append(.init(userInterfaceStyle: .dark))
+        }
+        
+        traitCollection = .init(traitsFrom: traits)
 #elseif os(macOS)
-        appearance = NSAppearance(named: .darkAqua)
+        switch settings.scheme {
+        case .auto:
+            break
+        case .light:
+            appearance = .init(named: .aqua)
+        case .dark:
+            appearance = .init(named: .darkAqua)
+        }
 #endif
         return self
     }
