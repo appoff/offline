e679e55784abc445a18e21802849d955d2c88c7c
vaux <zero.griffin@gmail.com>
2022-05-21 12:23:18 +0200

    Update coordinate

--- Sources/Coordinate.swift
+++ Sources/Coordinate.swift
@@ -1,12 +1,14 @@
 import CoreLocation
 import Archivable
 
+private let divider = Double(10_000_000)
+
 struct Coordinate: Storable {
-    let latitude: Double
-    let longitude: Double
+    let latitude: Int32
+    let longitude: Int32
     
     var coordinate: CLLocationCoordinate2D {
-        .init(latitude: latitude, longitude: longitude)
+        .init(latitude: .init(latitude) / divider, longitude: .init(longitude) / divider)
     }
     
     var data: Data {
@@ -16,8 +18,8 @@ struct Coordinate: Storable {
     }
     
     init(latitude: Double, longitude: Double) {
-        self.latitude = latitude
-        self.longitude = longitude
+        self.latitude = .init(latitude * divider)
+        self.longitude = .init(longitude * divider)
     }
     
     init(data: inout Data) {
