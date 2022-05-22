0e93935c62fb45dddf9e0f39905d21380625a203
vaux <zero.griffin@gmail.com>
2022-05-22 18:18:10 +0200

    Update add image

--- Sources/Factory.swift
+++ Sources/Factory.swift
@@ -59,7 +59,7 @@ public final class Factory {
             do {
                 let snapshot = try await shooter.start()
                 guard !canceled else { return }
-                try split(image: snapshot.image, shot: next)
+                try await split(image: snapshot.image, shot: next)
                 shots.removeLast()
                 
                 if shots.isEmpty {
@@ -88,64 +88,60 @@ public final class Factory {
     }
     
 #if os(iOS)
-    private func split(image: UIImage, shot: Shot) throws {
-        try (1 ..< shot.width)
-            .forEach { x in
-                try (0 ..< shot.height)
-                    .forEach { y in
-                        let size = Shot.tile * 2
-                        UIGraphicsBeginImageContext(.init(width: size, height: size))
-                        UIGraphicsGetCurrentContext()!.translateBy(x: 0, y: size)
-                        UIGraphicsGetCurrentContext()!.scaleBy(x: 1, y: -1)
-                        UIGraphicsGetCurrentContext()!.draw(image.cgImage!, in:
-                                .init(x: size * -.init(x),
-                                      y: (size * .init(y + 1)) - .init(image.cgImage!.height),
-                                      width: .init(image.cgImage!.width),
-                                      height: .init(image.cgImage!.height)))
-                        
-                         let data = UIImage(cgImage: UIGraphicsGetCurrentContext()!.makeImage()!)
-                            .jpegData(compressionQuality: 1)!
-                        
-                        UIGraphicsEndImageContext()
-                        
-                        try add(image: data,
-                                z: shot.z,
-                                x: x + shot.x,
-                                y: y + shot.y)
-                    }
+    private func split(image: UIImage, shot: Shot) async throws {
+        for x in 1 ..< shot.width {
+            for y in 0 ..< shot.height {
+                let size = Shot.tile * 2
+                UIGraphicsBeginImageContext(.init(width: size, height: size))
+                UIGraphicsGetCurrentContext()!.translateBy(x: 0, y: size)
+                UIGraphicsGetCurrentContext()!.scaleBy(x: 1, y: -1)
+                UIGraphicsGetCurrentContext()!.draw(image.cgImage!, in:
+                        .init(x: size * -.init(x),
+                              y: (size * .init(y + 1)) - .init(image.cgImage!.height),
+                              width: .init(image.cgImage!.width),
+                              height: .init(image.cgImage!.height)))
+                
+                let data = UIImage(cgImage: UIGraphicsGetCurrentContext()!.makeImage()!)
+                    .jpegData(compressionQuality: 1)!
+                
+                UIGraphicsEndImageContext()
+                
+                try await add(image: data,
+                              z: shot.z,
+                              x: x + shot.x,
+                              y: y + shot.y)
             }
+        }
     }
 #elseif os(macOS)
-    private func split(image: NSImage, shot: Shot) throws {
-        try (1 ..< shot.width)
-            .forEach { x in
-                try (0 ..< shot.height)
-                    .forEach { y in
-                        let tile = NSImage(size: .init(width: Shot.tile, height: Shot.tile))
-                        tile.lockFocus()
-                        image
-                            .draw(in: .init(x: 0, y: 0, width: Shot.tile, height: Shot.tile),
-                                  from: .init(x: Shot.tile * .init(x),
-                                              y: Shot.tile * .init(y),
-                                              width: Shot.tile,
-                                              height: Shot.tile),
-                                  operation: .copy,
-                                  fraction: 1)
-                        tile.unlockFocus()
-                        
-                        let data = NSBitmapImageRep(cgImage: tile.cgImage(forProposedRect: nil, context: nil, hints: nil)!)
-                            .representation(using: .png, properties: [:])!
-                        
-                        try add(image: data,
-                                z: shot.z,
-                                x: x + shot.x,
-                                y: shot.y + shot.height - y - 1)
-                    }
+    private func split(image: NSImage, shot: Shot) async throws {
+        for x in 1 ..< shot.width {
+            for y in 0 ..< shot.height {
+                let tile = NSImage(size: .init(width: Shot.tile, height: Shot.tile))
+                tile.lockFocus()
+                image
+                    .draw(in: .init(x: 0, y: 0, width: Shot.tile, height: Shot.tile),
+                          from: .init(x: Shot.tile * .init(x),
+                                      y: Shot.tile * .init(y),
+                                      width: Shot.tile,
+                                      height: Shot.tile),
+                          operation: .copy,
+                          fraction: 1)
+                tile.unlockFocus()
+                
+                let data = NSBitmapImageRep(cgImage: tile.cgImage(forProposedRect: nil, context: nil, hints: nil)!)
+                    .representation(using: .png, properties: [:])!
+                
+                try await add(image: data,
+                              z: shot.z,
+                              x: x + shot.x,
+                              y: shot.y + shot.height - y - 1)
             }
+        }
     }
 #endif
     
-    private func add(image: Data, z: Int, x: Int, y: Int) throws {
+    @MainActor private func add(image: Data, z: Int, x: Int, y: Int) throws {
         let data = Data()
             .adding(UInt32(image.count))
             .adding(image)
