1b8bf1a3f2c81740f82ee2b2e11bd5e0e65e3455
vaux <zero.griffin@gmail.com>
2022-05-25 12:48:57 +0200

    Add coord delta

--- Sources/CLLocationCoordinate2D.swift
+++ Sources/CLLocationCoordinate2D.swift
@@ -1,6 +1,10 @@
 import CoreLocation
 
 extension CLLocationCoordinate2D {
+    public func delta(other: Self) -> CLLocationDistance {
+        pow(latitude - other.latitude, 2) + pow(longitude - other.longitude, 2)
+    }
+    
     var coordinate: Coordinate {
         .init(latitude: latitude, longitude: longitude)
     }
