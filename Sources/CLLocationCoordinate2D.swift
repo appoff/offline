ed0b6a7479c27a286fcb481f2f3afd5919faa9a0
vaux <zero.griffin@gmail.com>
2022-05-25 12:57:21 +0200

    Update delta

--- Sources/CLLocationCoordinate2D.swift
+++ Sources/CLLocationCoordinate2D.swift
@@ -2,7 +2,7 @@ import CoreLocation
 
 extension CLLocationCoordinate2D {
     public func delta(other: Self) -> CLLocationDistance {
-        pow(latitude - other.latitude, 2) + pow(longitude - other.longitude, 2)
+        abs(latitude - other.latitude) + abs(longitude - other.longitude)
     }
     
     var coordinate: Coordinate {
