8faf410826eff120242b0f42bd46e8d217bf5e6e
vaux <zero.griffin@gmail.com>
2022-07-06 17:37:42 +0200

    Update decoding

--- Sources/String.swift
+++ Sources/String.swift
@@ -2,6 +2,6 @@ import Foundation
 
 extension String {
     var max8: Self {
-        String(decoding: Data(self.utf8).prefix(255), as: UTF8.self)
+        Self(decoding: Data(self.utf8).prefix(255), as: UTF8.self)
     }
 }
